62abed368985c35e38ee9b8004452ccc4095bbfd
Aman Bhardwaj <amanbhardwaj5931@gmail.com>
2025-05-01 17:29:27 +0530

    Install tailwind sucessfully and used it properly

--- health_connect/health_connect/settings.py
+++ health_connect/health_connect/settings.py
@@ -38,7 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'products',
+    'tailwind',
+    'theme',
+    'django_browser_reload',
+
 ]
+
+TAILWIND_APP_NAME = 'theme'
+
+INTERNAL_IPS = ['127.0.0.1']
+
+NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -48,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = 'health_connect.urls'
